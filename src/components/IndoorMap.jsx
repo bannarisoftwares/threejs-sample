a49ace2908828d9dc3d441cd811bd7c38282059a
natheeshkumar <rangasamyn@b2b.regn.net>
2023-08-09 18:09:54 +0530

    Update indoor map

--- src/components/IndoorMap.jsx
+++ src/components/IndoorMap.jsx
@@ -5,23 +5,49 @@ import {useLoader} from "@react-three/fiber";
 import {Environment, OrbitControls} from "@react-three/drei";
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import {Suspense} from "react";
+import {PerspectiveCamera} from '@react-three/drei'
 
 const Model = () => {
     const gltf = useLoader(GLTFLoader, "./demo-level.glb");
-    return (
-        <>
-            <primitive object={gltf.scene} scale={0.4}/>
-        </>
-    );
+    return (<>
+        <primitive object={gltf.scene} scale={0.4}/>
+    </>);
 };
 const IndoorMap = () => {
+    const d = 35;
+    const agentHeight = 1.0;
+    const agentRadius = 0.25;
     return (
         <Suspense fallback={null}>
+            <ambientLight intensity={0.5} color={"white"}/>
+            <directionalLight color="white" intensity={0.8} position={[20, 30, 0]} castShadow={true} shadow={{
+                mapSize: {
+                    width: 4096, height: 4096
+                },
+                camera: {
+                    left: -d,
+                    right: d,
+                    top: d,
+                    bottom: -d
+                }
+            }}/>
             <Model/>
-            <OrbitControls/>
-            <Environment preset="sunset" background/>
-        </Suspense>
-    );
+            <group position={[0, 1, 0]}>
+                <mesh position={[0, agentHeight / 2, 0]}>
+                    <cylinderGeometry args={[agentRadius, agentRadius, agentHeight]}/>
+                    <meshPhongMaterial attach="material" color={"green"}/>
+                </mesh>
+            </group>
+            <OrbitControls enablePan={true}
+                           enableDamping={true}
+                           minDistance={5}
+                           maxDistance={60}
+                           maxPolarAngle={Math.PI / 2 - 0.05}
+                           minPolarAngle={Math.PI / 4}/>
+            {/*<Environment preset="sunset" background/>*/}
+            <PerspectiveCamera fov={45} aspect={window.innerWidth / window.innerHeight} near={0.1} far={1000}
+                               position={[10, 10, 33]}/>
+        </Suspense>);
 };
 
 export default IndoorMap;
