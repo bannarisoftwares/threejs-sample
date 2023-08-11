d75e282838d688132b613f8dc97f8a7b3163b123
natheeshkumar <rangasamyn@b2b.regn.net>
2023-08-11 11:35:55 +0530

    Add lines
    Signed-off-by: natheeshkumar <rangasamyn@b2b.regn.net>

--- src/components/IndoorMap.jsx
+++ src/components/IndoorMap.jsx
@@ -1,5 +1,5 @@
 import React, {useMemo, useRef, useState} from 'react';
-import {useFrame} from "@react-three/fiber";
+import {extend, useFrame} from "@react-three/fiber";
 
 import {useLoader} from "@react-three/fiber";
 import {Environment, OrbitControls} from "@react-three/drei";
@@ -8,7 +8,12 @@ import {Suspense} from "react";
 import {PerspectiveCamera} from '@react-three/drei'
 import Button from "./Button";
 import {Pathfinding, PathfindingHelper} from "three-pathfinding";
-import * as THREE from "three";
+import {BufferAttribute} from "three";
+import {MeshLineGeometry, MeshLineMaterial} from 'meshline';
+import * as THREE from 'three';
+import {useControls} from 'leva'
+
+extend({MeshLineGeometry, MeshLineMaterial});
 
 
 const IndoorMap = () => {
@@ -17,6 +22,69 @@ const IndoorMap = () => {
     const agentRadius = 0.25;
     const ZONE = 'level1b';
 
+    const cNP = [
+        {
+            "x": 2.6,
+            "y": 5.1,
+            "z": -1.5
+        },
+        {
+            "x": 2.8,
+            "y": 5.1,
+            "z": -1.3
+        },
+        {
+            "x": 2.8,
+            "y": 5.1,
+            "z": 2.5
+        },
+        {
+            "x": 2.6,
+            "y": 5.1,
+            "z": 2.7
+        },
+        {
+            "x": -5.5,
+            "y": 5.1,
+            "z": 6
+        },
+        {
+            "x": -5.7,
+            "y": 5,
+            "z": 6.4
+        },
+        {
+            "x": -5.6,
+            "y": 5,
+            "z": 6.6
+        },
+        {
+            "x": 6.4,
+            "y": 1.1,
+            "z": 6.5
+        },
+        {
+            "x": 6.5,
+            "y": 1.1,
+            "z": 5.6
+        },
+        {
+            "x": 6.5,
+            "y": 1.1,
+            "z": 3.1
+        },
+        {
+            "x": 6.4,
+            "y": 1.1,
+            "z": 2.2
+        },
+        {
+            "x": 0,
+            "y": 1,
+            "z": 0
+        }
+    ]
+
     const navMeshGeometryRef = useRef({
         navMeshGeometry: null
     });
@@ -24,6 +92,41 @@ const IndoorMap = () => {
     const navPathRef = useRef({
         navPath: null
     });
+
+    const [navPath, setNavPath] = useState();
+
+    function Lines({dash, count, colors, radius = 50, rand = THREE.MathUtils.randFloatSpread}) {
+        const lines = useMemo(() => {
+            return Array.from({length: count}, () => {
+                const pos = new THREE.Vector3(rand(radius), rand(radius), rand(radius))
+                console.log("POS---->",pos)
+                const points = Array.from({length: 10}, () => pos.add(new THREE.Vector3(rand(radius), rand(radius), rand(radius))).clone())
+
+                console.log('points', points)
+                const curve = new THREE.CatmullRomCurve3(points).getPoints(300)
+                return {
+                    color: colors[parseInt(colors.length * Math.random())],
+                    width: Math.max(radius / 100, (radius / 50) * Math.random()),
+                    speed: Math.max(0.1, 1 * Math.random()),
+                    curve: curve.flatMap((point) => point.toArray())
+                }
+            })
+        }, [colors, count, radius])
+        return lines.map((props, index) => <Fatline key={index} dash={dash} {...props} />)
+    }
+
+    function Fatline({curve, width, color, speed, dash}) {
+        const ref = useRef()
+        useFrame((state, delta) => (ref.current.material.dashOffset -= (delta * speed) / 10))
+        return (
+            <mesh ref={ref}>
+                <meshLineGeometry points={curve}/>
+                <meshLineMaterial transparent lineWidth={width} color={color} depthWrite={false} dashArray={0.25}
+                                  dashRatio={dash} toneMapped={false}/>
+            </mesh>
+        )
+    }
+
 
     const onClick = () => {
         console.log("On Button clicked");
@@ -39,6 +142,7 @@ const IndoorMap = () => {
         const navpath = pathfinding.findPath(closest.centroid, target, ZONE, groupID);
         console.log("NAV path", navpath);
         navPathRef.current.navPath = navpath;
+        setNavPath(navPathRef.current.navPath)
     }
 
 
@@ -66,6 +170,98 @@ const IndoorMap = () => {
     };
 
 
+    function BufferPoints({count = 1000}) {
+        const OFFSET = 0.2;
+        const navPath = [
+            {
+                "x": 2.6,
+                "y": 5.1,
+                "z": -1.5
+            },
+            {
+                "x": 2.8,
+                "y": 5.1,
+                "z": -1.3
+            },
+            {
+                "x": 2.8,
+                "y": 5.1,
+                "z": 2.5
+            },
+            {
+                "x": 2.6,
+                "y": 5.1,
+                "z": 2.7
+            },
+            {
+                "x": -5.5,
+                "y": 5.1,
+                "z": 6
+            },
+            {
+                "x": -5.7,
+                "y": 5,
+                "z": 6.4
+            },
+            {
+                "x": -5.6,
+                "y": 5,
+                "z": 6.6
+            },
+            {
+                "x": 6.4,
+                "y": 1.1,
+                "z": 6.5
+            },
+            {
+                "x": 6.5,
+                "y": 1.1,
+                "z": 5.6
+            },
+            {
+                "x": 6.5,
+                "y": 1.1,
+                "z": 3.1
+            },
+            {
+                "x": 6.4,
+                "y": 1.1,
+                "z": 2.2
+            },
+            {
+                "x": 0,
+                "y": 1,
+                "z": 0
+            }
+        ]
+
+        const points = useMemo(() => {
+            const p = new Array(count).fill(0).map((v) => (0.5 - Math.random()) * 7.5);
+            return new BufferAttribute(new Float32Array(p), 3);
+        }, [count]);
+
+        console.log("POINTS...", points)
+
+        return (
+            <points>
+                <bufferGeometry>
+                    <bufferAttribute attach={"attributes-position"} {...points} />
+                </bufferGeometry>
+                <pointsMaterial
+                    size={0.1}
+                    threshold={0.1}
+                    color={0xff00ff}
+                    sizeAttenuation={true}
+                />
+            </points>
+        );
+    }
+
+    const {dash, count, radius} = useControls({
+        dash: {value: 0.9, min: 0, max: 0.99, step: 0.01},
+        count: {value: 50, min: 0, max: 200, step: 1},
+        radius: {value: 50, min: 1, max: 100, step: 1}
+    })
     return (<Suspense fallback={null}>
         <ambientLight intensity={0.5} color={"white"}/>
         <directionalLight color="white" intensity={0.8} position={[20, 30, 0]} castShadow={true} shadow={{
@@ -78,7 +274,10 @@ const IndoorMap = () => {
 
         <BaseModel/>
         <NavMeshModel/>
+        {/*<BufferPoints/>*/}
         {/*<Line/>*/}
+        <Lines dash={dash} count={count} radius={radius}
+               colors={[[10, 0.5, 2], [1, 2, 10], '#A2CCB6', '#FCEEB5', '#EE786E', '#e0feff']}/>
         <Button onClick={onClick}/>
         <group position={[0, 1, 0]}>
             <mesh position={[0, agentHeight / 2, 0]}>
